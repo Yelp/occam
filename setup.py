0026a36fc9aaff9e813401ebd17f3d43fea9bfe9
Fred Hatfull <fred.hatfull@gmail.com>
2014-03-18 10:16:31 -0700

    Bump to Flask 0.10.1

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     install_requires=[
         "celery[redis] == 3.1.9",
         "python-dateutil == 2.2",
-        "Flask == 0.10.0",
+        "Flask == 0.10.1",
         "requests == 2.2.0",
         "py_razor_client == 0.1.2",
         "pyyaml == 3.10",
